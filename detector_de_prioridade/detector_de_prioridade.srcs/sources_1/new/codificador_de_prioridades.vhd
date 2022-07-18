bb7cdd907e9cd216ac38109668a53d9d5ee84b97
Lucas Queiroz <86672557+lucasqueiroz23@users.noreply.github.com>
2022-07-18 10:46:18 -0300

    adicao da basys3 e inicio da codificacao

---
+++ detector_de_prioridade/detector_de_prioridade.srcs/sources_1/new/codificador_de_prioridades.vhd
@@ -0,0 +1,51 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 18.07.2022 10:33:30
+-- Design Name: 
+-- Module Name: codificador_de_prioridades - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity codificador_de_prioridades is
+    Port ( p0 : in STD_LOGIC;
+           p1 : in STD_LOGIC;
+           p2 : in STD_LOGIC;
+           p3 : in STD_LOGIC;
+           x0 : out STD_LOGIC;
+           x1 : out STD_LOGIC;
+           int : out STD_LOGIC);
+        
+end codificador_de_prioridades;
+
+architecture Behavioral of codificador_de_prioridades is
+
+begin
+    
+
+
+end Behavioral;
